3e3b5935d0b5c8514298cdb66f6c4520c6035e34
MatWilCode <growdice100@gmail.com>
2024-12-17 10:22:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+function showMessage() {
+    const message = document.getElementById('message');
+    message.textContent = 'Sigma Boy says: Be yourself and conquer!';
+    message.style.display = 'block';
+}
